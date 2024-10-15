9eca353e9806da8a340eb8888a7b4e54afbfb08a
Camilo-RBS <00083223@uca.edu.sv>
2024-10-15 10:21:12 -0600

    :zap: se subio todo de forma segura

---
+++ src/routes/user.route.js
@@ -0,0 +1,5 @@
+const { addUserController, showForm} = require('../controllers/user.controller')
+    const router = require('express').Router();
+    router.get('/', showForm);
+    router.post('/home', addUserController)
+    module.exports = router;
